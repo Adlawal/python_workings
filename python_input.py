1435ebf5b3166ab04ef38d3209df0aa0b566825a
adlawal <adetomiwa.lawal@gmail.com>
2026-02-17 21:34:06 +0100

    python input solution file added

---
+++ python_input.py
@@ -0,0 +1,11 @@
+# Write a simple Python program that takes a persons name as input and greet them
+# Write a simple Python program that takes a persons name and age as input. Then welcome the person and their age plus 20
+
+name =input("Enter name:")
+print(f"Hello {name},how are you doing?")
+
+age = int(input("How old are you?"))
+new_age=(age + 20)
+print(f"Hello {name}, you are {new_age} years old")
+
+
