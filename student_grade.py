ab0c5d776c90ab78e2c916614ef7cfd86941d869
adlawal <adetomiwa.lawal@gmail.com>
2026-02-19 22:26:09 +0100

    student grade added

--- student_grade.py
+++ student_grade.py
@@ -1,2 +1,8 @@
 #Write a Python script that prompts for a students name and last exam grade.
 #Add 50 to their score and divide their score by 4. Save the result. Then print their name and their result
+
+name= input("Enter your name:")
+print("how are you doing", name )
+Age= int(input("Enter your age:?"))
+Additional_age= Age + 20
+print(f"How are you doing {name}, you are {Additional_age}years old")
