d6c1eb78501b060d8a930c38a3922398aeed1b17
adlawal <adetomiwa.lawal@gmail.com>
2026-02-19 22:18:06 +0100

    student grade file added

---
+++ student_grade.py
@@ -0,0 +1,2 @@
+#Write a Python script that prompts for a students name and last exam grade.
+#Add 50 to their score and divide their score by 4. Save the result. Then print their name and their result
