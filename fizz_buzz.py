0b6ea028f6426d1ef1fb60eeda28db049ec4c610
adlawal <adetomiwa.lawal@gmail.com>
2026-02-19 23:10:07 +0100

    fizz buzz file added

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -12,7 +12,7 @@
 for number in range (1,101):
     if number % 3 == 0 and number % 5 ==0:
         print ("fizzBuzz")
-    if number % 3 == 0:
+    elif number % 3 == 0:
         print("fizz")
     elif number % 5 ==0:
         print ("buzz")
