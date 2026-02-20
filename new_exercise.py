d8eea20a33fd2562b92a384b03f97f612ca9c376
adlawal <adetomiwa.lawal@gmail.com>
2026-02-20 19:31:00 +0100

    new_exercise.py file added

---
+++ new_exercise.py
@@ -0,0 +1,17 @@
+
+# Given the list below
+# sales = [120,450,800,50,900,300]
+# Write a Python code that classifies items in the list as Low, Medium, or High. Also, do a count of items based on this classification and finally give a sum of items in each classification
+# Key: less than 300 – low
+#        > 300 <= 700 medium
+# > 700 that's high
+
+# From temperatures = [30,22,35,19,40], print those above average.
+
+
+even_numbers = [12, 7, 9, 20, 33, 14, 5]
+for even_number in even_numbers (12, 7, 9, 20, 33, 14, 5) :
+  if even_number % 2 == 0 :
+    print  ["even_number"]
+
+
