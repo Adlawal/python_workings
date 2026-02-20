a65838f7f870b77e89aedd8f6e3c06ddb3faba6f
adlawal <adetomiwa.lawal@gmail.com>
2026-02-20 22:20:33 +0100

    even number solution added

---
+++ even number.py
@@ -0,0 +1,17 @@
+#Given numbers = [12, 7, 9, 20, 33, 14, 5], print only even numbers and store them in a new list.
+
+numbers = [12, 7, 9, 20, 33, 14, 5]
+even_number=[]
+for num in numbers :
+    if num % 2 == 0:
+        even_number.append(num)
+print(even_number)
+
+
+
+
+
+
+
+
+
